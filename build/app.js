056a58157f5dfb556fbbbcff963d8f753bc639fe
Patrick Marabeas <patrick@marabeas.io>
2014-05-22 21:56:20 +1000

    Updated names missed

--- build/app.js
+++ build/app.js
@@ -4,7 +4,7 @@
 
 'use strict';
 
-React.renderComponent( FitText( {text:"ng-FitText.js", degree:"2", min:"30"} ), document.getElementById('ft1') );
+React.renderComponent( FitText( {text:"react-FitText.js", degree:"2", min:"30"} ), document.getElementById('ft1') );
 React.renderComponent( FitText( {text:"FitText", degree:".315"} ), document.getElementById('ft2') );
 React.renderComponent( FitText( {text:"rustled my jimmies", degree:".76"} ), document.getElementById('ft3') );
 React.renderComponent( FitText( {text:"a React rework of the original", degree:"3", min:"20"} ), document.getElementById('ft4') );
