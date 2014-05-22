78c1865877df124e1f6379e072b117ac37347a92
Patrick Marabeas <patrick@marabeas.io>
2014-05-22 20:34:06 +1000

    Initial release

---
+++ build/app.js
@@ -0,0 +1,10 @@
+/**
+ * @jsx React.DOM
+ */
+
+'use strict';
+
+React.renderComponent( FitText( {text:"ng-FitText.js", degree:"2", min:"30"} ), document.getElementById('ft1') );
+React.renderComponent( FitText( {text:"FitText", degree:".315"} ), document.getElementById('ft2') );
+React.renderComponent( FitText( {text:"rustled my jimmies", degree:".76"} ), document.getElementById('ft3') );
+React.renderComponent( FitText( {text:"a React rework of the original", degree:"3", min:"20"} ), document.getElementById('ft4') );
---
+++ build/react-FitText.js
@@ -0,0 +1,46 @@
+/**
+ * @jsx React.DOM
+ *
+ * react-FitText.js v0.0.1
+ * https://github.com/patrickmarabeas/react-FitText.js
+ *
+ * Original jQuery project: https://github.com/davatron5000/FitText.js
+ * AngularJS version: https://github.com/patrickmarabeas/ng-FitText.js
+ *
+ * Copyright 2014, Patrick Marabeas http://marabeas.io
+ * Released under the MIT license
+ * http://opensource.org/licenses/mit-license.php
+ *
+ * Date: 22/05/2014
+ */
+
+'use strict';
+
+var FitText = React.createClass({displayName: 'FitText',
+  render: function() {
+    var text = this.props.text || 'error: specify text';
+    return React.DOM.span(null, text);
+  },
+  componentDidMount: function() {
+    var elem = this.getDOMNode();
+        elem.style.display = 'block';
+    var degree = this.props.degree || 1;
+    var min = this.props.min || Number.NEGATIVE_INFINITY;
+    var max = this.props.max || Number.POSITIVE_INFINITY;
+
+    var resizer = function() {
+      var width = elem.offsetWidth;
+      elem.style.fontSize = Math.max(
+        Math.min(
+            width / ( degree * 10 ),
+          parseFloat( max )
+        ),
+        parseFloat( min )
+      ) + 'px';
+    }; resizer();
+
+    window.addEventListener("resize", function() {
+      resizer();
+    });
+  }
+});
